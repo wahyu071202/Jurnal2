f7594ffee5c2d5b81663d3194d9691c9ee2a5c78
wahyuo71202 <you@example.com>
2020-09-14 07:34:55 +0700

    Hellomiami.java

---
+++ tugas IA1/Hellomiami.java
@@ -0,0 +1,9 @@
+public class Hellomiami{
+   
+   public static void main(String[] args) {
+      System.out.println("Nama: M wahyu indra wardana");
+	  System.out.println("Nim : 6706201137");
+	  System.out.println("D3IF4404");
+	}
+
+}
